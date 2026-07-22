be35fb25af0da9b8540fa530b5c38d3a9ef522a9
nopal <muhammad.240180162@mhs.unimal.ac.id>
2026-07-23 00:12:20 +0700

    Use relative asset paths /js/... and enforce HTTPS server flag to completely eliminate Mixed Content errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,14 @@ class AppServiceProvider extends ServiceProvider
         $host = $_SERVER['HTTP_HOST'] ?? '';
         if ($host && !in_array($host, ['localhost', '127.0.0.1', '127.0.0.1:8000', 'localhost:8000'])) {
             URL::forceScheme('https');
+            if ($this->app->has('request')) {
+                $this->app['request']->server->set('HTTPS', 'on');
+            }
         } elseif (config('app.env') === 'production' || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
             URL::forceScheme('https');
+            if ($this->app->has('request')) {
+                $this->app['request']->server->set('HTTPS', 'on');
+            }
         }
     }
 }
