c8b3ec05310e8ec06c642d8fa6c0eabe551906d3
nopal <muhammad.240180162@mhs.unimal.ac.id>
2026-07-23 04:36:52 +0700

    vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,28 @@
 <?php
 
+// Create required writable storage directories in Vercel Serverless /tmp environment
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0777, true);
+    }
+}
+
+// Bind Vercel /tmp storage environment variables
+putenv('APP_STORAGE=/tmp/storage');
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+$_SERVER['APP_STORAGE'] = '/tmp/storage';
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
 // Forward Vercel requests to Laravel public/index.php
 require __DIR__ . '/../public/index.php';
